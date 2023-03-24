4d8b2fc7a6d013feb685c32050b62c10f6a50840
Toby131 <leehj94131@gmail.com>
2023-03-24 13:22:55 +0900

    멤버 리파지토리, 컨트롤러 등

---
+++ Hello-spring/src/main/java/Hello/Hellospring/controller/MemberController.java
@@ -0,0 +1,16 @@
+package Hello.Hellospring.controller;
+
+import Hello.Hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+}
---
+++ Hello-spring/src/test/java/Hello/Hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,12 @@
+package Hello.Hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
