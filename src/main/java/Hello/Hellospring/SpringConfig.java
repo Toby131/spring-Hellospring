590ca901856b91fabc62940f2de83d032ceafcfe
Toby131 <leehj94131@gmail.com>
2023-04-12 13:17:25 +0900

    using JPA (solved error)

--- src/main/java/Hello/Hellospring/SpringConfig.java
+++ src/main/java/Hello/Hellospring/SpringConfig.java
@@ -1,43 +1,45 @@
 package Hello.Hellospring;
 
-import Hello.Hellospring.repository.JdbcMemberRepository;
-import Hello.Hellospring.repository.JdbcTemplateMemberRepository;
-import Hello.Hellospring.repository.JpaMemberRepository;
 import Hello.Hellospring.repository.MemberRepository;
 import Hello.Hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class SpringConfig {
 
-    //private DataSource dataSource;
+    /*private DataSource dataSource;
 
-    /*public SpringConfig(DataSource dataSource){
+    public SpringConfig(DataSource dataSource){
         this.dataSource = dataSource;
     }*/
 
-    private EntityManager em;
+   /* private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
+    }*/
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        //return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource)
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        //return new MemoryMemberRepository();
+//        //return new JdbcMemberRepository(dataSource)
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        //return new JpaMemberRepository(em);
+//    }
 }
